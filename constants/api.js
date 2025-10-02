20d935bad51ebbde3590e9fe8f4085e70fe023fe
Karolina <michajlowkarolina@gmail.com>
2025-10-02 12:09:15 +0200

    Finish

---
+++ constants/api.js
@@ -0,0 +1,3 @@
+// if you're using your physical phone, change this to the deployed url
+// we have explained this in the course :-)
+export const API_URL = "https://recipe-app-w2x9.onrender.com/api";
